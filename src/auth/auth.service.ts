235d127b3994ade77ee147b93c06a23cddbdd2a9
VitorDalBelo <vitoralmeida721@gmail.com>
2023-07-22 17:08:03 -0300

    Login Mutation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { LoginUserInput } from './dto/login-user.input';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService){}
-
+    constructor(private usersServices: UsersService){}
+    
     async validadeUser(username:string,password:string):Promise<any> {
-        const user = await this.usersService.findOne(username);
-
+        const user = await this.usersServices.findOne(username);
+        
         if(user && user.password == password){
             const {password,...result} =user;
             return result;
         } 
         return null ;
+    }
+    async login(loginUserInput: LoginUserInput) {
+
+        const user = await this.usersServices.findOne(loginUserInput.username);
+        const {password,...result} =user;
+
+        return{
+            access_token:"jwt",
+            user:result
+        };
     }
 }
