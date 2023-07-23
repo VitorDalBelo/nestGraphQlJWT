339b76e308b8c582145f9cf52cb969a6aee23623
VitorDalBelo <vitoralmeida721@gmail.com>
2023-07-23 16:30:14 -0300

    jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,13 @@ import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports:[PassportModule,UsersModule,JwtModule.register({
     signOptions:{expiresIn:'60s'},
     secret:"hide-me"
   })],
-  providers: [AuthService, AuthResolver,LocalStrategy]
+  providers: [AuthService, AuthResolver,LocalStrategy,JwtStrategy]
 })
 export class AuthModule {}
