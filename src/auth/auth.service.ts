68e7a4e45d1a8c65f4297af42ff319a82e53ed89
VitorDalBelo <vitoralmeida721@gmail.com>
2023-07-23 17:57:43 -0300

    fim da aula do japones

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserInput } from './dto/login-user.input';
+import * as bcrypt from "bcrypt"
 
 @Injectable()
 export class AuthService {
@@ -10,8 +11,9 @@ export class AuthService {
     
     async validadeUser(username:string,password:string):Promise<any> {
         const user = await this.usersServices.findOne(username);
-        
-        if(user && user.password == password){
+        if(!user) return null;
+        const checkHash = await bcrypt.compare(password , user.password)
+        if(user.password == password || checkHash){
             const {password,...result} =user;
             return result;
         } 
@@ -28,7 +30,9 @@ export class AuthService {
     async signup(loginUserInput: LoginUserInput) {
         const user = await this.usersServices.findOne(loginUserInput.username)
         if(user) throw new Error('User already exixists');
-        return this.usersServices.create({...loginUserInput,})
+
+        const password = await bcrypt.hash(loginUserInput.password,10);
+        return this.usersServices.create({...loginUserInput,password})
 
     }
 }
